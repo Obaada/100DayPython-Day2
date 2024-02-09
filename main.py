fc6c6c5153c352950dc7370ba0ce457493c7ecbd
Obaada <159489903+Obaada@users.noreply.github.com>
2024-02-09 23:20:23 +0000

    2

--- main.py
+++ main.py
@@ -14,3 +14,28 @@ print(123 + 345)
 #Boolean
 True
 False
+
+
+# Casting
+Age = input("What is your age? ")
+print("Your age is " + str(Age) + " years old.")
+
+print(70 + float("100.5"))
+
+print(str(70) +  str(100))
+
+# Mathematical Operations
+2 + 2
+2 - 2
+3 * 5
+5 ** 2
+10 / 2
+
+# PEMDAS    
+# ()
+# **
+# * /
+# + -
+print(3 * (3 + 3) / 3 - 3)
+
+# Rounding`
